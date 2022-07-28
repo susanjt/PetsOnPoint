fe8d1241b39f7df2ce4224852430078c26ae7fa2
Susan Thompson <suejim74@gmail.com>
2022-07-27 21:30:34 -0400

    updated js, package files

---
+++ contact.js
@@ -0,0 +1,8 @@
+let pattern = new RegExp("([!#-'*+/-9=?A-Z^-~-]+(\.[!#-'*+/-9=?A-Z^-~-]+)*|\"\(\[\]!#-[^-~ \t]|(\\[\t -~]))+\")@([!#-'*+/-9=?A-Z^-~-]+(\.[!#-'*+/-9=?A-Z^-~-]+)*|\[[\t -Z^-~]*])");
+
+function checkEmailAddress(email_address) {
+  let result = pattern.test(email_address);
+  console.log(result);
+}
+
+checkEmailAddress();
--- main.js
+++ main.js
@@ -11,11 +11,3 @@ function closeMenu() {
 navLinks.classList.toggle ('active');
 }
 
-let pattern = new RegExp("([!#-'*+/-9=?A-Z^-~-]+(\.[!#-'*+/-9=?A-Z^-~-]+)*|\"\(\[\]!#-[^-~ \t]|(\\[\t -~]))+\")@([!#-'*+/-9=?A-Z^-~-]+(\.[!#-'*+/-9=?A-Z^-~-]+)*|\[[\t -Z^-~]*])");
-
-function checkEmailAddress(email_address) {
-  let result = pattern.test(email_address);
-  console.log(result);
-}
-
-checkEmailAddress('suejim74@gmail.com');
