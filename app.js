52b4f77d2c30e73caaff4d9995371279133e497b
Susan Thompson <suejim74@gmail.com>
2022-08-01 09:11:48 -0400

    updated files, testing

--- app.js
+++ app.js
@@ -16,31 +16,4 @@
   })
   Client.Instance.accountName; //=> 'account'
 
-  const express = require('express');
-const cp = require('cookie-parser');
-const app = express();
-app.use(cp());
 
-app.get('/set', (req, res) => {
-  // Set the new style cookie
-  res.cookie('3pcookie', 'value', { sameSite: 'none', secure: true });
-  // And set the same value in the legacy cookie
-  res.cookie('3pcookie-legacy', 'value', { secure: true });
-  res.end();
-});
-
-app.get('/', (req, res) => {
-  let cookieVal = null;
-
-  if (req.cookies['3pcookie']) {
-    // check the new style cookie first
-    cookieVal = req.cookies['3pcookie'];
-  } else if (req.cookies['3pcookie-legacy']) {
-    // otherwise fall back to the legacy cookie
-    cookieVal = req.cookies['3pcookie-legacy'];
-  }
-
-  res.end();
-});
-
-app.listen(process.env.PORT);
--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 window.scrollTo({left:0, top:0, behavior: `smooth`})
 
 
-const toggleButton = document.getElementById('paw');
+const toggleButton = document.getElementById('hamburger');
 const navLinks = document.getElementById('navLinks');
 toggleButton.addEventListener('click', () => {
- navLinks.classList.paw('active')
+ navLinks.classList.toggle('active')
 });
 
 function closeMenu() {
